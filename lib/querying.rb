3e475e489d13291493a564de7c258a8c49ba0b31
Warren Yao <warrenyca905@gmail.com>
2019-08-01 00:47:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_series_title_with_most_human_characters
   FROM series
   LEFT JOIN characters ON characters.series_id = series.id
   WHERE characters.species = 'human'
-  ORDER BY COUNT(characters.species)
+  ORDER BY characters.species
   LIMIT 1;"
 end
 
