524428d30e46a24533a8a856c62df80b7dbc5d36
Warren Yao <warrenyca905@gmail.com>
2019-08-01 00:34:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,8 +29,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT title
-  FROM series
-  LEFT JOIN "
+  FROM series;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
